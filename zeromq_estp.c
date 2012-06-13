877c71490d6dd47d2f1e77735f51e249b6ad327f
Paul Colomiets <paul@colomiets.name>
2012-06-14 02:39:08 +0300

    Implement multiple-value types for input
    The implementation uses cache as a temporary storage for values, and
    adds values to cache one by one

--- zeromq_estp.c
+++ zeromq_estp.c
@@ -24,12 +24,11 @@
  *   Julien Ammous
  **/
 
-#define _XOPEN_SOURCE /* strptime */
-#define _BSD_SOURCE /* snprintf */
+#define _GNU_SOURCE
 #include "collectd.h"
 #include "common.h" /* auxiliary functions */
 #include "plugin.h" /* plugin_register_*, plugin_dispatch_values */
-#include "utils_cache.h"
+#include "utils_avltree.h"
 #include "network.h"
 
 /* for htons() */
@@ -49,8 +48,20 @@ struct cmq_socket_s {
 };
 typedef struct cmq_socket_s cmq_socket_t;
 
+struct cmq_cache_entry_s {
+    char *fullname;
+    char *timestamp;
+    char *type;
+    char *items;
+    int nitems;
+    unsigned long mask;
+    value_t values[];
+};
+typedef struct cmq_cache_entry_s cmq_cache_entry_t;
+
 static int cmq_threads_num = 1;
 static void *cmq_context = NULL;
+static c_avl_tree_t *staging = NULL;
 
 static pthread_t *receive_thread_ids = NULL;
 static size_t     receive_thread_num = 0;
@@ -66,92 +77,19 @@ static void cmq_close_callback (void *socket) /* {{{ */
         (void) zmq_close (socket);
 } /* }}} void cmq_close_callback */
 
-static void parse_message (char *data, int dlen)
-{
-
-    char fullname[300];
-    char timestamp[32];
-    unsigned long interval;
-    char value[64];
-
-    char *endline = memchr (data, '\n', dlen);
-    if(endline)
-        dlen = endline - data;
-    char *ndata = alloca(dlen+1);
-    memcpy(ndata, data, dlen);
-    ndata[dlen] = 0;
-    data = ndata;
-
-    int rc = sscanf (data, "ESTP:%300s %31s %lu %63s",
-                     fullname, timestamp, &interval, value);
-    if (rc != 4) {
-        WARNING("ZeroMQ-ESTP: message has wrong format");
-        return;
-    }
-
+static void dispatch_entry(char *fullname, char *timestamp, time_t interval,
+                           char *vtype, int values_len, value_t *values) {
     struct tm timest;
+    value_list_t vl;
+    strcpy(vl.type, vtype);
+
     if (!strptime (timestamp, "%Y-%m-%dT%H:%M:%S", &timest)) {
         WARNING("ZeroMQ-ESTP: can't parse timestamp");
         return;
     }
-
-    int vdouble = 0;
-    double dvalue;
-    char *end;
-    long lvalue = strtol (value, &end, 10);
-
-    if(*end == '.') {
-        dvalue = strtod(value, &end);
-        vdouble = 1;
-    }
-    if(end == data) {
-        WARNING("ZeroMQ-ESTP: wrong value");
-        return;
-    }
-
-    value_list_t vl;
-    value_t val;
-
-
-    if(*end == ':') {
-        ++end;
-        if(*end == 'd') {
-            strcpy(vl.type, "derive");  // FIXME: derive can be negative
-            if(vdouble) {
-                val.derive = dvalue;
-            } else {
-                val.derive = lvalue;
-            }
-        } else if(*end == 'c') {
-            strcpy(vl.type, "derive");  // non-wrapping counter
-            if(vdouble) {
-                val.counter = dvalue;
-            } else {
-                val.counter = lvalue;
-            }
-        } else if(*end == 'a') {
-            strcpy(vl.type, "absolute");
-            if(vdouble) {
-                val.absolute = dvalue;
-            } else {
-                val.absolute = lvalue;
-            }
-        } else {
-            WARNING("ZeroMQ-ESTP: Unknown type");
-            return;
-        }
-    } else {
-        strcpy(vl.type, "gauge");
-        if(vdouble) {
-            val.gauge = dvalue;
-        } else {
-            val.gauge = lvalue;
-        }
-    }
-
     // Hostname
     char *cpos = fullname;
-    end = strchr(cpos, ':');
+    char *end = strchr(cpos, ':');
     if(!end) {
         WARNING("ZeroMQ-ESTP: No delimiter after hostname");
         return;
@@ -207,12 +145,237 @@ static void parse_message (char *data, int dlen)
 
     vl.time = TIME_T_TO_CDTIME_T(timegm(&timest));
     vl.interval = TIME_T_TO_CDTIME_T(interval);
-    vl.values = &val;
-    vl.values_len = 1;
+    vl.values = values;
+    vl.values_len = values_len;
     vl.meta = NULL;
 
     plugin_dispatch_values (&vl);
+}
 
+static char *find_suffix(char *fullname) {
+    char *endsuffix = fullname + strlen(fullname)-1;
+    if(*endsuffix != ':') {
+        WARNING("ZeroMQ-ESTP: Metric full name not ends with ':'");
+        return NULL;
+    }
+    char *suffix = endsuffix-1;
+    while(suffix > fullname) {
+        if(*suffix == '.' || *suffix == ':') {
+            return suffix+1;
+        }
+        --suffix;
+    }
+    return NULL;
+}
+
+static int find_field(char *suffix, char *items) {
+    int suffixlen = strlen(suffix) - 1  /* colon at the end */;
+    int idx = 0;
+    if(!strncmp(suffix, items, suffixlen)
+       && (items[suffixlen] == 0 || items[suffixlen] == ',')) {
+        return 0;
+    }
+    char *c;
+    for(c = items; *c; ++c) {
+        if(*c == ',') {
+            c += 1;
+            idx += 1;
+            if(!strncmp(suffix, c, suffixlen)
+               && (c[suffixlen] == 0 || c[suffixlen] == ',')) {
+                return idx;
+            }
+        }
+    }
+    WARNING("ZeroMQ-ESTP: Can't find suffix in list of suffixes");
+    return -1;
+}
+
+static void dispatch_multi_entry (char *fullname, char *timestamp,
+                                  time_t interval,
+                                  char *type, char *items,
+                                  value_t value) {
+    char *suffix = find_suffix(fullname);
+    if(!suffix)
+        return;
+    int myindex = find_field(suffix, items);
+    if(myindex < 0)
+        return;
+
+    int clen = suffix-fullname+2;
+    char cutname[clen];
+    memcpy(cutname, fullname, clen-2);
+    cutname[clen-2] = ':';
+    cutname[clen-1] = 0;
+
+    cmq_cache_entry_t *entry;
+    int rc = c_avl_get (staging, cutname, (void *)&entry);
+
+    if(rc == 0) {
+        if(!strcmp(entry->timestamp, timestamp)
+           && !strcmp(entry->type, type)
+           && !strcmp(entry->items, items)) {
+            entry->values[myindex] = value;
+            entry->mask |= 1 << myindex;
+            if(entry->mask == (1 << entry->nitems)-1) {
+                dispatch_entry(cutname, timestamp, interval, type,
+                               entry->nitems, entry->values);
+                c_avl_remove(staging, cutname, NULL, NULL);
+                free(entry);
+            }
+            return;
+        } else {
+            INFO("ZeroMQ-ESTP: Clearing cache entry as it is stale\n");
+            c_avl_remove(staging, cutname, NULL, NULL);
+            free(entry);
+        }
+    }
+    // Ok, let's create new entry
+    int tlen = strlen(timestamp) + 1;
+    int ylen = strlen(type) + 1;
+    int ilen = strlen(items) + 1;
+    int nitems = 1;
+    char *c;
+    for(c = items; *c; ++c)
+        if(*c == ',')
+            nitems += 1;
+    entry = malloc(sizeof(cmq_cache_entry_t)
+        + nitems*sizeof(value_t)
+        + clen + tlen + ylen + ilen);
+    if(!entry) {
+        WARNING("Not enough memory for new entry");
+        return;
+    }
+    char *curbuf = ((char *)entry) + sizeof(cmq_cache_entry_t)
+                                   + nitems*sizeof(value_t);
+    memcpy(curbuf, cutname, clen);
+    entry->fullname = curbuf;
+    curbuf += clen;
+    memcpy(curbuf, timestamp, tlen);
+    entry->timestamp = curbuf;
+    curbuf += tlen;
+    memcpy(curbuf, type, ylen);
+    entry->type = curbuf;
+    curbuf += ylen;
+    memcpy(curbuf, items, ilen);
+    entry->items = curbuf;
+    curbuf += ilen;
+    entry->nitems = nitems;
+    entry->mask = 1 << myindex;
+    entry->values[myindex] = value;
+
+    c_avl_insert(staging, entry->fullname, entry);
+}
+
+static void parse_message (char *data, int dlen)
+{
+
+    char fullname[300];
+    char timestamp[32];
+    unsigned long interval;
+    char value[64];
+
+    char *endline = memchr (data, '\n', dlen);
+    int headlen = dlen;
+    if(endline)
+        headlen = endline - data;
+    char *hdata = alloca(headlen+1);
+    memcpy(hdata, data, headlen);
+    hdata[headlen] = 0;
+
+    int rc = sscanf (hdata, "ESTP:%300s %31s %lu %63s",
+                     fullname, timestamp, &interval, value);
+    if (rc != 4) {
+        WARNING("ZeroMQ-ESTP: message has wrong format");
+        return;
+    }
+
+    int vdouble = 0;
+    double dvalue;
+    char *end;
+    long lvalue = strtol (value, &end, 10);
+
+    if(*end == '.') {
+        dvalue = strtod(value, &end);
+        vdouble = 1;
+    }
+    if(end == hdata) {
+        WARNING("ZeroMQ-ESTP: wrong value");
+        return;
+    }
+
+    char vtype[64];
+    value_t val;
+
+    // Parse type
+    if(*end == ':') {
+        ++end;
+        if(*end == 'd') {
+            strcpy(vtype, "derive");  // FIXME: derive can be negative
+            if(vdouble) {
+                val.derive = dvalue;
+            } else {
+                val.derive = lvalue;
+            }
+        } else if(*end == 'c') {
+            strcpy(vtype, "derive");  // non-wrapping counter
+            if(vdouble) {
+                val.counter = dvalue;
+            } else {
+                val.counter = lvalue;
+            }
+        } else if(*end == 'a') {
+            strcpy(vtype, "absolute");
+            if(vdouble) {
+                val.absolute = dvalue;
+            } else {
+                val.absolute = lvalue;
+            }
+        } else {
+            WARNING("ZeroMQ-ESTP: Unknown type");
+            return;
+        }
+    } else {
+        strcpy(vtype, "gauge");
+        if(vdouble) {
+            val.gauge = dvalue;
+        } else {
+            val.gauge = lvalue;
+        }
+    }
+
+    //  Let's find extension
+    if(endline) { //  there is extension data
+        char *ext = memmem(endline, dlen - headlen, "\n :collectd:", 12);
+        if(ext) { //  collectd extension found
+            ext += 12;
+            char *endext = memchr(ext, '\n', data + dlen - ext);
+            int elen;
+            if(!endext)
+                elen = data + dlen - ext;
+            else
+                elen = endext - ext;
+            char *fullext = alloca(elen+1);
+            memcpy(fullext, ext, elen);
+            fullext[elen] = 0;
+            while (42) {
+                char ekey[24];
+                char evalue[elen];
+                int chars;
+                if(sscanf(fullext, " %23[^=]=%s%n", ekey, evalue, &chars) < 2)
+                    break;
+                fullext += chars;
+                if(!strcmp(ekey, "type")) {
+                    strncpy(vtype, evalue, 64);
+                } else if(!strcmp(ekey, "items")) {
+                    dispatch_multi_entry(fullname, timestamp, interval,
+                                         vtype, evalue, val);
+                    return;
+                }
+            }
+        }
+    }
+
+    dispatch_entry(fullname, timestamp, interval, vtype, 1, &val);
 }
 
 static void *receive_thread (void *cmq_socket) /* {{{ */
@@ -252,7 +415,8 @@ static void *receive_thread (void *cmq_socket) /* {{{ */
 #define PACKET_SIZE   512
 
 static int put_single_value (void *socket, const char *name, value_t value,
-                             const value_list_t *vl, const data_source_t *ds)
+                             const value_list_t *vl, const data_source_t *ds,
+                             char *extdata)
 {
     int datalen;
     char data[640];
@@ -264,21 +428,21 @@ static int put_single_value (void *socket, const char *name, value_t value,
     strftime(tstring, 32, "%Y-%m-%dT%H:%M:%S", &tstruct);
 
     if(ds->type == DS_TYPE_COUNTER) {
-        datalen = snprintf(data, 640, "ESTP:%s:%s:%s:%s: %s %d %llu:c",
+        datalen = snprintf(data, 640, "ESTP:%s:%s:%s:%s: %s %d %llu:c%s",
             vl->host, vl->plugin, vl->plugin_instance, name,
-            tstring, interval, value.counter);
+            tstring, interval, value.counter, extdata);
     } else if(ds->type == DS_TYPE_GAUGE) {
-        datalen = snprintf(data, 640, "ESTP:%s:%s:%s:%s: %s %d %lf",
+        datalen = snprintf(data, 640, "ESTP:%s:%s:%s:%s: %s %d %lf%s",
             vl->host, vl->plugin, vl->plugin_instance, name,
-            tstring, interval, value.gauge);
+            tstring, interval, value.gauge, extdata);
     } else if(ds->type == DS_TYPE_DERIVE) {
-        datalen = snprintf(data, 640, "ESTP:%s:%s:%s:%s: %s %d %ld:d",
+        datalen = snprintf(data, 640, "ESTP:%s:%s:%s:%s: %s %d %ld:d%s",
             vl->host, vl->plugin, vl->plugin_instance, name,
-            tstring, interval, value.derive);
+            tstring, interval, value.derive, extdata);
     } else if(ds->type == DS_TYPE_ABSOLUTE) {
-        datalen = snprintf(data, 640, "ESTP:%s:%s:%s:%s: %s %d %lu:a",
+        datalen = snprintf(data, 640, "ESTP:%s:%s:%s:%s: %s %d %lu:a%s",
             vl->host, vl->plugin, vl->plugin_instance, name,
-            tstring, interval, value.absolute);
+            tstring, interval, value.absolute, extdata);
     } else {
         WARNING("ZeroMQ-ESTP: Unknown type");
         return -1;
@@ -314,6 +478,22 @@ static int write_value (const data_set_t *ds, /* {{{ */
     assert (vl->values_len == ds->ds_num);
     if(ds->ds_num > 1) {
         int i;
+
+        int elen = 100; // prefix and type
+        for(i = 0; i < ds->ds_num; ++i)
+            elen += strlen(ds->ds[i].name) + 1;
+        char *extdata = alloca(elen);
+        char *cur = extdata + sprintf(extdata,
+            "\n :collectd: type=%s items=%s",
+            ds->type, ds->ds[0].name);
+        for(i = 1; i < ds->ds_num; ++i) {
+            *cur++ = ',';
+            int len = strlen(ds->ds[i].name);
+            memcpy(cur, ds->ds[i].name, len);
+            cur += len;
+        }
+        *cur = 0;
+
         for(i = 0; i < ds->ds_num; ++i) {
             if(*vl->type_instance) {
                 char name[64];
@@ -322,15 +502,22 @@ static int write_value (const data_set_t *ds, /* {{{ */
                     vl->type_instance, ds->ds[i].name);
                 name[len] = 0;
                 put_single_value(user_data->data,
-                    name, vl->values[i], vl, &ds->ds[i]);
+                    name, vl->values[i], vl, &ds->ds[i], extdata);
             } else {
                 put_single_value(user_data->data,
-                    ds->ds[i].name, vl->values[i], vl, &ds->ds[i]);
+                    ds->ds[i].name, vl->values[i], vl, &ds->ds[i], extdata);
             }
         }
     } else {
+        char extdata[100] = "";
+        if(strcmp(ds->type, "gauge")
+           && strcmp(ds->type, "counter")
+           && strcmp(ds->type, "derive")
+           && strcmp(ds->type, "absolute")) {
+            sprintf(extdata, "\n :collectd: type=%s", ds->type);
+        }
         put_single_value(user_data->data,
-            vl->type_instance, vl->values[0], vl, &ds->ds[0]);
+            vl->type_instance, vl->values[0], vl, &ds->ds[0], extdata);
     }
     return 0;
 }
@@ -569,6 +756,11 @@ static int plugin_init (void)
     int major, minor, patch;
     zmq_version (&major, &minor, &patch);
     INFO("ZeroMQ plugin loaded (zeromq v%d.%d.%d).", major, minor, patch);
+    staging = c_avl_create ((void *) strcmp);
+    if (staging == NULL) {
+        ERROR("ZeroMQ-ESTP : c_avl_create failed: %s", strerror(errno));
+        return -1;
+    }
     return 0;
 }
 
@@ -586,6 +778,23 @@ static int my_shutdown (void)
         }
 
         pthread_join(listen_thread_id, NULL);
+
+    }
+
+    if( staging ) {
+        while (42) {
+            char *key = NULL;
+            cmq_cache_entry_t *value = NULL;
+
+            int rc = c_avl_pick (staging, (void *) &key, (void *) &value);
+            if (rc != 0)
+                break;
+
+            free (key);
+            free (value);
+        }
+
+        c_avl_destroy (staging);
     }
 
     return 0;
